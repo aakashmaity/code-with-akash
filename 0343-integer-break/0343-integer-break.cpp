5b15b43529d8009a5ec40d1ae8f5be55839e3062
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2023-10-06 17:27:28 -0700

    Time: 2 ms (50.23%), Space: 6.7 MB (11.91%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n<=3)
+            return n-1;
+
+        int q=n/3;
+        int rem=n%3;
+        if(rem==0){
+            return pow(3,q);
+        }
+        else if(rem==1){
+            return pow(3,q-1)*4;
+        }
+        else{
+            return pow(3,q)*2;
+        }
+    }
+};
