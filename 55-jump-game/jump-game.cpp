9db75d6b444416e4eec6948b8992a20e117a0d8f
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-01-02 21:38:42 +0530

    Time: 1731 ms (5.01%) | Memory: 51.7 MB (7.91%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    bool solve(vector<int>& nums,int pos,int n,vector<int>&dp)
+    {
+        if(pos==n)
+            return true;
+        if(pos>n)
+            return false;
+        
+        if(dp[pos]!= -1)
+            return dp[pos];
+
+        for(int i=1;i<=nums[pos];i++)
+        {
+            if(solve(nums,pos+i,n,dp))
+            {
+                // dp[pos]= true;
+                return true;
+            }
+        }   
+        return dp[pos]=false;
+    }
+    bool canJump(vector<int>& nums) {
+        int n=nums.size()-1;
+        vector<int> dp(n+1,-1);
+        return solve(nums,0,n,dp);
+    }
+};
