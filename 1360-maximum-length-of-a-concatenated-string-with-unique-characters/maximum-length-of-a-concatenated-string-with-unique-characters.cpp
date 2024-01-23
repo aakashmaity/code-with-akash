67860eb57c647f235a27e4923d1c1c53588c44a9
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-01-23 23:41:55 +0530

    Time: 60 ms (52.43%) | Memory: 59.5 MB (39.71%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    
+    int check(string output)
+    {
+        vector<int>freq(26,0);
+        for(auto i: output)
+        {
+            if(freq[i-'a']!=0){
+                return 0;
+            }
+            freq[i-'a']++;
+        }
+        return output.size();
+    }
+    void solve(vector<string>& arr,int i,string output,int &ans)
+    {
+        if(i>=arr.size())
+        {
+            return;
+        }
+        
+        //exclude 
+        solve(arr,i+1,output,ans);
+
+        
+        output+=arr[i];
+        int res=check(output);
+        ans=max(ans,res);
+        //include
+        if(res)
+            solve(arr,i+1,output,ans);
+    }
+    int maxLength(vector<string>& arr) {
+        int n = arr.size();
+        int ans=0;
+        string output="";
+        solve(arr,0,output,ans);
+        return ans;
+    }
+};
