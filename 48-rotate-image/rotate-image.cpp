ec9097d476f1de69d98cb1ce98eecae3e40e437f
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-02-09 01:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (23.07%) - LeetSync

--- 48-rotate-image/rotate-image.cpp
+++ 48-rotate-image/rotate-image.cpp
@@ -3,7 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         int n = matrix.size();
 
-        //transpose of a matrix
+        //transpose the matrix
         for(int i=0;i<n-1;i++)
         {
             for(int j=i+1;j<n;j++)
