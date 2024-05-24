70127d605e9419e93d62350021a2474e4a37034f
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-05-25 05:13:01 +0530

    Time: 4 ms (80.78%) | Memory: 16.8 MB (74.17%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.cpp
@@ -1,36 +1,44 @@
 class Solution {
 public:
+    int maxScore=INT_MIN;
+    void solve(int i, int currScore, vector<string>& words, vector<int>& score,vector<int>& freq)
+    {
+        maxScore = max(maxScore, currScore);
+        if(i>=words.size())
+            return;
+        
+        int j=0;
+        int tempScore =0;
+        vector<int>tempfreq = freq;
+        int flag=1;
+        while( j<words[i].size() )
+        {
+            char ch=words[i][j];
+            tempfreq[ch-'a']--;
+            tempScore += score[ch-'a'];
+
+            if(tempfreq[ch-'a']<0){
+                flag=0;
+                break;
+            }
+            j++;
+        }
+    
+        // Take that word
+        if(flag)
+        {
+            solve(i+1,currScore+tempScore,words,score,tempfreq);
+        }
+
+        // Not take the word
+        solve(i+1,currScore,words,score,freq);
+    }
     int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
-        vector<int> freqAv(26);
-        for(auto ch : letters){
-            freqAv[ch - 'a']++;
-        }
-        int maxScore = 0;
-        vector<int> currSubNeeds(26);
-        int size = words.size();
-        for(int indxMask = 1; indxMask < (1 << size); indxMask++){
-            currSubNeeds.assign(26, 0);
-            for(int indx = 0; indx < size; indx++){
-                if((indxMask & (1 << indx)) != 0){
-                    string &currWrd = words[indx];
-                    for(auto chr : currWrd){
-                        currSubNeeds[chr - 'a']++;
-                    }
-                }
-            }
-            bool notPos = false;
-            int scoreTotal = 0;
-            for(int indx = 0; indx < 26; indx++){
-                if(currSubNeeds[indx] > freqAv[indx]){
-                    notPos = true;
-                    break;
-                }else{
-                    scoreTotal += score[indx] * currSubNeeds[indx];
-                }
-            }
-            if(!notPos)
-                maxScore = max(maxScore, scoreTotal);
-        }
+        vector<int>freq(26,0);
+        for(auto &x : letters)
+            freq[x-'a']++;
+        
+        solve(0,0,words,score,freq); // index, currentScore
         return maxScore;
     }
 };
