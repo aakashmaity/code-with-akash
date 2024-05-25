8411ef8534ad8e40946ed9f95e56cd30fc76f5ef
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-05-25 23:11:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (32.97%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> ans;
+    unordered_set<string> dict;
+    void solve(int i,string s,string currSentence){
+        if(i>=s.size()){
+            ans.push_back(currSentence);
+            return;
+        }
+        for(int j=i;j<s.length();j++)
+        {
+            string tempWord = s.substr(i,j-i+1);
+            if(dict.count(tempWord))
+            {
+                string tempSentence = currSentence;
+
+                if(!currSentence.empty()){
+                    currSentence += " ";
+                }
+                
+                currSentence += tempWord;   // take word
+                solve(j+1,s,currSentence);  // explore
+                currSentence = tempSentence;  // removed tempWord;
+            }
+        }
+
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        
+        for(string &str : wordDict){
+            dict.insert(str);
+        }
+        string currSentence = "";
+        solve(0,s,currSentence);
+        return ans;
+    }
+};
