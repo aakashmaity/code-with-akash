1b439c5dfe41d9a5a7be6800080fe775278ae28d
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-06-02 09:39:30 +0530

    Time: 316 ms (16.67%) | Memory: 125.6 MB (16.67%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& m) {
+        int n = m.size();
+        sort(m.begin(), m.end());
+        int count = 0;
+        count += abs(m[0][0] - 1);
+        for(int i = 1; i < n; i++){
+            if(m[i][0] <= m[i-1][1]){
+                if(m[i][1] < m[i-1][1]) 
+                m[i][1] = m[i - 1][1];
+            }
+            else{
+                int dy = m[i][0] - m[i-1][1];
+                count += dy - 1; 
+            }
+        }
+        count += abs(m[n-1][1] - days);
+        return count;
+    }
+};
