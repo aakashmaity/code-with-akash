a92422a46c474504b15baa616c31c0769d0ef1b4
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-06-27 22:30:43 +0530

    Time: 3 ms (37.15%) | Memory: 7.8 MB (93.42%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == INT_MAX) {
+            if(x==1){
+                return 1;
+            }
+            else{
+                if(x==-1){
+                    return -1;
+                }
+                else{
+                    return 0;
+                }
+            }
+        }
+        if (n == INT_MIN) {
+            if(x==1 || x==-1){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        
+        double ans=1;
+        if(n >= 0){
+            while(n--){
+                ans *= x;
+            }
+        }
+        else{
+            while(n<0){
+                ans /=x;
+                n++;
+            }
+        }
+        return ans;
+    }
+};
