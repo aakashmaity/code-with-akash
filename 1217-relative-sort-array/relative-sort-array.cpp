58271b0db6ccf54fdc04477f6f5f7aa75a507d2e
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-07-13 23:38:44 +0530

    Time: 3 ms (51.66%) | Memory: 11.2 MB (5.09%) - LeetSync

--- 1217-relative-sort-array/relative-sort-array.cpp
+++ 1217-relative-sort-array/relative-sort-array.cpp
@@ -1,26 +1,30 @@
 class Solution {
 public:
     vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
-        int n = arr1.size();
-        int m = arr2.size();
-        vector<int> freq(1001,0);
-        for(int i=0;i<n;i++){
-            freq[arr1[i]]++;
+        unordered_map<int,int> mp;
+
+        // store the order of sorting using index.
+        // 2->0, 1->1, 4->2, 3->3, 9->4, 6->5
+        for(int i=0;i<arr2.size();i++){
+            mp[arr2[i]] = i;
         }
-        vector<int>ans;
-        for(int i=0;i<m;i++)
-        {
-            while(freq[arr2[i]]--){
-                ans.push_back(arr2[i]);
+
+        // 7->1e9, 19->1e9
+        for(int num : arr1){
+            if(!mp.count(num)){
+                mp[num] = 1e9;
             }
         }
-        for(int i=0;i<1001;i++){
-            if(freq[i]>0){
-                while(freq[i]--){
-                    ans.push_back(i);
-                }
+        
+        // using '&' (in [&]) we can acces outer veriable
+        auto lambda = [&](int &num1, int &num2){
+            if(mp[num1] == mp[num2]){  // num1->ie9, num2->1e9
+                return num1 < num2;
             }
-        }
-        return ans;
+            return mp[num1] < mp[num2];
+        };
+
+        sort(begin(arr1),end(arr1),lambda);
+        return arr1;
     }
 };
