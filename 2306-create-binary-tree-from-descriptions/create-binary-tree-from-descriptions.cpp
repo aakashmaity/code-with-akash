0fad4d898dc3408ee7cda6a729288fa56c888089
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-07-15 21:06:48 +0530

    Time: 639 ms (90.18%) | Memory: 256.3 MB (92.58%) - LeetSync

--- 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -6,60 +6,30 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
-        int n = descriptions.size();
         unordered_map<int, TreeNode*> mp;
-        unordered_set<int> st;
 
-        for (vector<int>& node : descriptions) {
-            int parent = node[0];
-            int child = node[1];
-            int side = node[2];
-
-            TreeNode* temp;
-            // parent
-            if (!mp.count(parent)) {
-                TreeNode* newNode = new TreeNode(parent);
-                mp[parent] = newNode;
-                temp = newNode;
+        for (vector<int> &node : descriptions) {
+            TreeNode* parent = mp.count(node[0]) ? mp[node[0]] : new TreeNode(node[0]);
+            TreeNode* child = mp.count(node[1]) ? mp[node[1]] : new TreeNode(node[1]);
+            
+            if (node[2]) {
+                parent->left = child;
             } else {
-                temp = mp[parent];
+                parent->right = child;
             }
 
-            // child
-            if (!mp.count(child)) {
-                if (side) { // Left
-                    temp->left = new TreeNode(child);
-                    mp[child] = temp->left;
-                } else { // Right
-                    temp->right = new TreeNode(child);
-                    mp[child] = temp->right;
-                }
-            } else {
-                TreeNode* ch = mp[child];
-                if(side){
-                    temp -> left = ch;
-                }
-                else{
-                    temp->right = ch;
-                }
-            }
-            st.insert(child);
+            mp[node[0]] = parent;
+            mp[node[1]] = child;
         }
-
-        TreeNode *ans = NULL;
-        for(vector<int>& node : descriptions){
-            if(!st.count(node[0])){
-                ans = mp[node[0]];
-                break;
-            }
+        for (vector<int> &node : descriptions) {
+            mp.erase(node[1]);
         }
-        return ans;
+        return mp.begin()->second;
     }
 };
