1cfc2c1df24c2c5195c669988ea4bbb0df4bcc2e
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-07-29 22:25:43 +0530

    Time: 28 ms (78.08%) | Memory: 11.6 MB (51.74%) - LeetSync

--- 1511-count-number-of-teams/count-number-of-teams.cpp
+++ 1511-count-number-of-teams/count-number-of-teams.cpp
@@ -4,18 +4,38 @@ public:
         int n = rating.size();
 
         int ans=0;
-        for(int i=0;i<n-2;i++){
-            for(int j=i+1;j<n-1;j++){
-                bool increase = rating[i] < rating[j];
-                for(int k=j+1;k<n;k++){
-                    if(increase && rating[j] < rating[k]){
-                        ans++;
-                    }
-                    else if(!increase && rating[j] > rating[k]){
-                        ans++;
-                    }
+        for(int j=1;j<n-1;j++){
+            // for increasing count
+            int leftsmaller = 0;
+            int rightgreater = 0;
+            
+            // for decreasing count
+            int leftgreater = 0;
+            int rightsmaller = 0;
+
+
+            // finding for left side
+            for(int i=j-1;i>=0;i--){
+                if(rating[i] < rating[j]){
+                    leftsmaller++;
+                }
+                else if(rating[i] > rating[j]){
+                    leftgreater++;
                 }
             }
+
+
+            // finding for right side
+            for(int k=j+1;k<n;k++){
+                if(rating[j] < rating[k]){
+                    rightgreater++;
+                }
+                else if(rating[j] > rating[k]){
+                    rightsmaller++;
+                }
+            }
+
+            ans += (leftsmaller * rightgreater) + (leftgreater * rightsmaller);
         }
         return ans;
     }
