c7eebf4431f0ce8c92dbdc5166e88d2127925a59
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-08-05 00:09:01 +0530

    Time: 75 ms (84.05%) | Memory: 11.5 MB (81.35%) - LeetSync

--- 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
+++ 1615-range-sum-of-sorted-subarray-sums/range-sum-of-sorted-subarray-sums.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
-    int MOD = 1e9 + 7;
+    typedef pair<int,int> P;  // subArray sum, end index of this subArray
+    int MOD = 1e9+7;
+
     int rangeSum(vector<int>& nums, int n, int left, int right) {
+        priority_queue< P, vector<P> , greater<P> > pq;
         
-        vector<int>subarrSum;
         for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                subarrSum.push_back(sum);
-            }
+            pq.push({nums[i],i});
         }
-        sort(begin(subarrSum),end(subarrSum));
-        int ans=0;
-        for(int i=left-1;i<right;i++){
-            ans = (ans + subarrSum[i]) % MOD;
+
+        int ans = 0;
+
+        for(int count = 1;count<=right;count++){
+            auto p = pq.top();
+            pq.pop();
+
+            int sum = p.first;
+            int idx = p.second;
+
+            if(count >= left){
+                ans = (ans + sum) % MOD;
+            }
+
+            P new_pair;
+
+            int new_idx = idx+1;
+            if(new_idx < n){
+                new_pair.first = sum+nums[new_idx];
+                new_pair.second = new_idx;
+                pq.push(new_pair);
+            }
         }
         return ans;
     }
