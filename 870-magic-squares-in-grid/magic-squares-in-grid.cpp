b3f2891440d555cfb1e9f3d7af0bcd0f1f77e633
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-08-09 21:19:37 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (18.49%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -3,45 +3,36 @@ public:
     bool isMagicSquare(vector<vector<int>> &grid, int row,int col){
         unordered_set<int>st;
 
-        vector<int> rowSum(3,0);
-        int k=0;
-        int sum = grid[row][col] + grid[row][col+1] + grid[row][col+2];
-        cout<<row<<" " <<col<<" "<<sum<<endl;
-    
-        for(int i=row;i<=row+2;i++){
-            int currColsum = 0;
-            for(int j=col;j<=col+2;j++){
-                
-                // check element is in b/w 0-9 
-                if(grid[i][j] <= 0 || grid[i][j] > 9){
+        // check unique and check 1-9
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                int num = grid[row+i][col+j];
+                if(num <= 0 || num > 9 || st.count(num)){
                     return false;
                 }
-
-                // check unique or not
-                if(st.count(grid[i][j])){
-                    return false;
-                }
-                st.insert(grid[i][j]);
-                
-                currColsum += grid[i][j];     // cols sums
-
-                rowSum[k++] += grid[i][j];    // rows sums
-                k = k%3;
+                st.insert(num);
             }
-            // check col sums
-            if(currColsum != sum){
+        }
+
+        int sum = grid[row][col] + grid[row][col+1] + grid[row][col+2];
+        for(int i=0;i<3;i++){
+            // check col
+            if(grid[row+i][col] + grid[row+i][col+1] + grid[row+i][col+2] != sum){
+                return false;
+            }
+
+            // check row
+            if(grid[row][col+i] + grid[row+1][col+i] + grid[row+2][col+i] != sum){
                 return false;
             }
         }
-        // check row sums
-        if(rowSum[0] != rowSum[1] || rowSum[1] != rowSum[2] || rowSum[0] != sum){
+
+        // check left digonal
+        if(grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2] != sum){
             return false;
         }
-
-        // check digonals
-        int leftdigo = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2];
-        int rightdigo = grid[row][col+2] + grid[row+1][col+1] + grid[row+2][col];
-        if( leftdigo != rightdigo || leftdigo != sum){
+        // check right digonal
+        if(grid[row][col+2] + grid[row+1][col+1] + grid[row+2][col] != sum){
             return false;
         }
         return true;
