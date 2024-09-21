cca2e6946b9d03bd86bfdc639cd5fc95f2fafc0e
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-09-21 20:54:21 +0530

    Time: 15 ms (11.33%) | Memory: 10 MB (83.04%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.cpp
+++ 214-shortest-palindrome/shortest-palindrome.cpp
@@ -1,35 +1,17 @@
 class Solution {
 public:
     string shortestPalindrome(string s) {
-        if(s == "") return s;
         string rev = s;
         reverse(rev.begin(), rev.end());
-        string str = s+"@"+rev;
 
-        //now we find lps array
-        int n = str.length();
-        vector<int> lps(n, 0);
-        int i = 0; 
-        for (int j = 1; j < n; ) {
-            if (str[i] == str[j]) {
-                lps[j] = i + 1;
-                i++;
-                j++;
-            } else {
-                if (i != 0) {
-                    i = lps[i - 1]; // fall back to prev lps
-                } else {
-                    lps[j] = 0; // no prefix match move forward
-                    j++;
-                }
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            if (!memcmp(s.c_str(), rev.c_str() + i, n - i)){
+                return rev.substr(0,i)+s;
             }
         }
-        // cout<<str<<endl;
-        // for(auto it: lps) cout<<it<<" ";
-        // cout<<endl<<i<<endl;
-        string temp = s.substr(i);
-        // cout<<temp;
-        reverse(temp.begin(), temp.end());
-        return temp+s;
+        return rev+s;
     }
 };
+
+
