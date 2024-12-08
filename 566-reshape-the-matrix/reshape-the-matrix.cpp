78c66118cc739db61f7df47d13fea9764b920314
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-12-08 21:21:42 +0530

    Time: 71 ms (6.57%) | Memory: 15.7 MB (5.55%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -2,12 +2,6 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         vector<vector<int>>ans(r,vector<int>(c));
-        // for(int i=0;i<r;i++){
-        //     for(int j=0;j<c;j++){
-        //         cout<<ans[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
         int n=mat.size();
         int m=mat[0].size();
 
