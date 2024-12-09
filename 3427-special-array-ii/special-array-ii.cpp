fae125834b57b11ae7ce6d4f0615db21900b7b1f
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2024-12-09 12:30:05 +0530

    Time: 48 ms (18.36%) | Memory: 137.7 MB (8.68%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+    vector<bool> isArraySpecial(vector<int>& nums,vector<vector<int>>& queries) {
         int n = nums.size();
-        int m = queries.size();
-        vector<int>converted(1,0);
-        vector<bool>ans;
+        vector<int>parityCounter(1,0);
+
         int k=0;
-        for(int i=1;i<n;i++)
-        {
+        for(int i=1;i<n;i++){
             if(nums[i]%2 == nums[i-1]%2)
                 k++;
-            converted.push_back(k);
+            parityCounter.push_back(k);
         }
-        for(int i=0;i<m;i++)
-        {
-            ans.push_back(converted[queries[i][0]] == converted[queries[i][1]]);
+
+        vector<bool>ans;
+        for(vector<int>q : queries){
+            ans.push_back(parityCounter[q[0]] == parityCounter[q[1]]);
         }
+
         return ans;
     }
 };
