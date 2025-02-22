10fa5a9b1d5c3b4e4ff43ad19c225ce19aa9a603
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2025-02-22 22:59:24 +0530

    Time: 3 ms (81.61%), Space: 17.8 MB (62.86%) - LeetHub

--- 2881-split-strings-by-separator/2881-split-strings-by-separator.py
+++ 2881-split-strings-by-separator/2881-split-strings-by-separator.py
@@ -1,9 +1,10 @@
 class Solution:
     def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
-        res = []
-        for item in words:
-            temp = item.split(separator)
-            temp = filter(lambda x: len(x) != 0, temp)
-            res.extend(temp)
-        
-        return res
+        ans = []
+
+        for word in words:
+            for str in word.split(separator):
+                if str:
+                    ans.append(str)
+            
+        return ans
