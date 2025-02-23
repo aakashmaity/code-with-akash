42b9f0c0ee86f1b8b4b67f0537083393593fdfb8
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2025-02-23 13:51:55 +0530

    Time: 0 ms (100%), Space: 18.5 MB (92.09%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,13 +1,18 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
-        arr = sorted(nums)
-        
-        if arr[0] != 0:
-            return 0
+        ans = 0
 
-        for i in range(1,n):
-            if arr[i-1]+1 != arr[i]:
-                return arr[i-1]+1
-            
-        return n
+        for i in range(1, n+1):
+            ans ^= i
+        
+        for item in nums:
+            ans ^= item
+        return ans
+
+
+        # [3,0,1]
+        # first loop , ans = 0^1^2^3
+        # second loop, ans = 0^1^2^3 ^ elements of nums
+        #              ans = 0^1^2^3 ^ 3^0^1
+        #              ans = 2
