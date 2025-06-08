d2d7ece6fc7a6155af565fcfbdd5746163b3b317
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2025-06-08 23:23:36 +0530

    Time: 0 ms (100%), Space: 17.8 MB (52.42%) - LeetHub

---
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -0,0 +1,17 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        s, e = 0, n
+        mid = 0
+        while(s<=e):
+            mid = s+(e-s)//2
+            totalCoins = mid*(mid+1)//2
+            if(totalCoins == n):
+                return mid
+            if(totalCoins > n):
+                e = mid - 1
+            else:
+                s = mid + 1
+        return e
+        
+            
+        
