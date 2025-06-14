19dc20a2444e4ea962ddd019038581bc711b6927
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2025-06-14 21:07:52 +0530

    Time: 0 ms (100%), Space: 18 MB (11.69%) - LeetHub

--- 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -5,7 +5,7 @@ class Solution:
         max_val = s
         for ch in s:
             if ch != '9':
-                max_val = s.replace(ch, '9')  # Replace each occarance of 'ch' with '9'
+                max_val = s.replace(ch, '9')  # Replace each occurrence of 'ch' with '9'
                 break
 
         min_val = s
