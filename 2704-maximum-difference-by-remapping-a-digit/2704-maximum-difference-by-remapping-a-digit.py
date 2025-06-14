6096144958ee9d80087ee04ee7220646be43c063
AKASH MAITY <113245103+aakashmaity@users.noreply.github.com>
2025-06-14 21:07:09 +0530

    Time: 0 ms (100%), Space: 17.9 MB (29.44%) - LeetHub

---
+++ 2704-maximum-difference-by-remapping-a-digit/2704-maximum-difference-by-remapping-a-digit.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minMaxDifference(self, num: int) -> int:
+        s = str(num)
+
+        max_val = s
+        for ch in s:
+            if ch != '9':
+                max_val = s.replace(ch, '9')  # Replace each occarance of 'ch' with '9'
+                break
+
+        min_val = s
+        for ch in s:
+            if ch != '0':
+                min_val = s.replace(ch, '0')
+                break
+        
+        return int(max_val) - int(min_val)
+
+        
